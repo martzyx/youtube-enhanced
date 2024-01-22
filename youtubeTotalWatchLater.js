702e58fd067f96b6838ff317054b2a2c769eed64
Martzyx <114746510+Martzyx@users.noreply.github.com>
2024-01-22 22:00:51 +0100

    improved timing by observing progress bar animation finish

--- youtubeTotalWatchLater.js
+++ youtubeTotalWatchLater.js
@@ -46,16 +46,53 @@ function sumDurations(durations) {
     return `${hours} hours, ${minutes} minutes, ${seconds} seconds`;
 }
 
-setTimeout(() => {
+function buildTimeTotal() {
     const totalDuration = sumDurations(findTimes());
-    console.log(totalDuration + " is the total time of your playlist");
-    // creating div for textcontent and appending
-    var timeDiv = document.createElement("div");
-    timeDiv.style.color = "var(--yt-spec-text-secondary)";
-    timeDiv.style.marginLeft = "1em";
-    timeDiv.textContent = `Total: ${totalDuration}`;
-    var publisherContainer = document.querySelectorAll("#publisher-container");
-    publisherContainer.forEach((el) => {
-        el.appendChild(timeDiv);
+    console.log("youtube-watch-total: " + totalDuration + " is the total time of your playlist");
+    if (!document.querySelector("#timeDisplay")) {
+        var timeDiv = document.createElement("div"); // creating div for textcontent and appending
+        timeDiv.id = "timeDisplay";
+        timeDiv.style.color = "var(--yt-spec-text-secondary)";
+        timeDiv.style.marginLeft = "1em";
+        timeDiv.textContent = `Total: ${totalDuration}`;
+        var publisherContainer = document.querySelectorAll("#publisher-container");
+        publisherContainer.forEach((el) => {
+            el.appendChild(timeDiv);
+        });
+    } else {
+        timeDisplayEl = document.querySelector("#timeDisplay");
+        timeDisplayEl.textContent = `Total: ${totalDuration}`;
+    }
+}
+
+function finishLoadPage() {
+    buildTimeTotal();
+}
+
+function observeProgressElement(progressElement) {
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (let mutation of mutationsList) {
+            if (mutation.type === "attributes" && mutation.attributeName === "hidden") {
+                finishLoadPage();
+            }
+        }
     });
-}, 5000);
+
+    observer.observe(progressElement, { attributes: true });
+}
+
+function observeBodyElement() {
+    const bodyObserver = new MutationObserver((mutationsList, observer) => {
+        const progressElement = document.querySelector("yt-page-navigation-progress");
+        if (progressElement) {
+            observer.disconnect(); // stop observing the body
+            observeProgressElement(progressElement); // start observing the progress element
+        }
+    });
+
+    bodyObserver.observe(document.body, { childList: true, subtree: true });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    observeBodyElement();
+});
